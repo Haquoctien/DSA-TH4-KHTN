647debb3b85c193b25796859f9451b47aacbd42c
Hà Quốc Tiến <17521122@ms.uit.edu.vn>
2018-04-09 19:13:37 +0700

    bai3

--- DSA-TH4-KHTN/bai2/Source.cpp
+++ DSA-TH4-KHTN/bai2/Source.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "../DSA-TH4-KHTN/SLL.h"
 using namespace std;
-intNodeList decimalToBinary(int);
 int main(void) {
 	cout << "Nhap mot so thap phan de chuyen so nay ve nhi phan: ";
 	int n;
@@ -10,18 +9,6 @@ int main(void) {
 	listOutput(ans);
 	system("pause");
 	return 0;
-}
-
-intNodeList decimalToBinary(int x)
-{
-	intNodeList list;
-	intNodeListInit(list);
-	while (x > 0)
-	{
-		addHead(list, newNode(x % 2));
-		x /= 2;
-	}
-	return list;
 }
 
 
